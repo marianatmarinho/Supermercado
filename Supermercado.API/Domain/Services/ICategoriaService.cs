19cfcb892d0b7fe1ffb21ade9d3d90fa4a16e091
mariana <mariana.ti@plasutil.com.br>
2021-05-21 17:58:42 -0300

    criação API Categorias

---
+++ Supermercado.API/Domain/Services/ICategoriaService.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Supermercado.API.Domain.Models;
+
+namespace Supermercado.API.Domain.Services
+{
+    public interface ICategoriaService
+    {
+         Task<IEnumerable<Categoria>> ListAsync();
+         
+    }
+}
