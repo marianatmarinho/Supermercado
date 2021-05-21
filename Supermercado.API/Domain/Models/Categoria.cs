29fab47d985460167508bf3445b77ae37e23bd1f
mariana <mariana.ti@plasutil.com.br>
2021-05-21 17:34:46 -0300

    criação do modelo de domínio

---
+++ Supermercado.API/Domain/Models/Categoria.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace Supermercado.API.Domain.Models
+{
+    public class Categoria
+    {
+        public int Id {get; set;}
+
+        public string Nome { get; set; }
+
+        public IList<Produto> Produtos {get; set;} = new List<Produto>();
+         
+    }
+}
